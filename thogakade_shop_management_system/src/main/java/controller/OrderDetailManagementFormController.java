a5af14e6033aa24abcfa85e5b96a6c687abff7bd
rashmiwijemanna <rashmiwijemanna7@gmail.com>
2025-09-19 17:44:09 +0530

    add orderDetailManagement ui , OrderDetailManagementFormController class, OrderDetailManagementController, OrderDetailManagementService interface

---
+++ thogakade_shop_management_system/src/main/java/controller/OrderDetailManagementFormController.java
@@ -0,0 +1,67 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class OrderDetailManagementFormController {
+
+    @FXML
+    private Button ADD;
+
+    @FXML
+    private Button CLEAR;
+
+    @FXML
+    private Button DELETE;
+
+    @FXML
+    private Button UPDATE;
+
+    @FXML
+    private TableColumn<?, ?> colDiscount;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colOrderId;
+
+    @FXML
+    private TableColumn<?, ?> colOrderQty;
+
+    @FXML
+    private TextField txtDiscount;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtOrderId;
+
+    @FXML
+    private TextField txtOrderQty;
+
+    @FXML
+    void btnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdate(ActionEvent event) {
+
+    }
+
+}
